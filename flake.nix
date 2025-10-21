d5ee0124c0c789a1e7b45fcfcfb3e10cfb67f756
Sócrates Díaz <sdsxd1800@gmail.com>
2025-10-20 21:22:33 -0400

    feat: use flake utils to cover each platform for violetland package

--- flake.nix
+++ flake.nix
@@ -3,13 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs {system = "x86_64-linux"; };
+      pkgs = nixpkgs.legacyPackages.${system};
     in
       {
-        packages.x86_64-linux.default = pkgs.callPackage ./violetland.nix { };
-      };
+        packages = rec {
+          violetland = pkgs.callPackage ./violetland.nix { };
+          default = violetland;
+        };
+      }
+  );
 }
